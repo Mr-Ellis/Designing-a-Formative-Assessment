ff063d4334d9e56d1bd97cfdd5908a5dbfa9f3de
Mr-Ellis <151756107+Mr-Ellis@users.noreply.github.com>
2026-05-13 14:27:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function startEscape() {
-  window.location.href = "puzzles/node1.html";
+  // Go directly to node1.html in the root folder
+  window.location.href = "node1.html";
 }
 
 function goToNode(node) {
+  // Navigate to node2.html, node3.html, etc.
   window.location.href = `node${node}.html`;
 }
 
 function checkCode(inputId, correctCode, nextNode) {
   const userCode = document.getElementById(inputId).value.trim();
+
   if (userCode === correctCode) {
     alert("Access Granted. Door Unlocked.");
-    goToNode(nextNode);
+
+    // If nextNode is 0, return to index.html (optional)
+    if (nextNode === 0) {
+      window.location.href = "index.html";
+    } else {
+      goToNode(nextNode);
+    }
+
   } else {
     alert("Access Denied. Try again.");
   }
