a3255d290c38abda2211c78063905ce573757d1a
Mr-Ellis <151756107+Mr-Ellis@users.noreply.github.com>
2026-05-13 14:31:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function startEscape() {
-  // Go directly to node1.html in the root folder
   window.location.href = "node1.html";
 }
 
 function goToNode(node) {
-  // Navigate to node2.html, node3.html, etc.
   window.location.href = `node${node}.html`;
 }
 
@@ -14,7 +12,6 @@ function checkCode(inputId, correctCode, nextNode) {
   if (userCode === correctCode) {
     alert("Access Granted. Door Unlocked.");
 
-    // If nextNode is 0, return to index.html (optional)
     if (nextNode === 0) {
       window.location.href = "index.html";
     } else {
@@ -25,3 +22,4 @@ function checkCode(inputId, correctCode, nextNode) {
     alert("Access Denied. Try again.");
   }
 }
+
